3dd700cecaa202c4f9a96657271e3de8b6134013
AbirDoesFrontend <ak.cr852456@gmail.com>
2025-01-23 01:58:14 +0600

    Node: CommonJs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-// run `node index.js` in the terminal
+const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(generateRandomNumber());
+console.log(celciusToFahrenheit(0));
